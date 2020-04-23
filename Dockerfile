4ed1c0d5a7f073ffb5b3dc8b4d03664451fb6186
shanakaray <shanakaray@yahoo.com>
2020-04-23 16:05:33 +0800

    initial commit
    initial commit

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ MAINTAINER Name <shanakaray@yahoo.com>
 
 VOLUME /tmp
 ARG JAR_FILE
-ADD ${JAR_FILE} contact.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/contact.jar"]
+ADD ${JAR_FILE} contact-trace-api.jar
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/contact-trace-api.jar"]
--- src/main/java/com/yra/contact/trace/ContactTraceApiApplication.java
+++ src/main/java/com/yra/contact/trace/ContactTraceApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactTraceApplication {
+public class ContactTraceApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContactTraceApplication.class, args);
+		SpringApplication.run(ContactTraceApiApplication.class, args);
 	}
 
 }
